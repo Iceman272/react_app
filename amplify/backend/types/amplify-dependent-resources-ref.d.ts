cd5e4b9ece2020e2c54eee28426e0f52097a39ea
Iceman272 <72137901+Iceman272@users.noreply.github.com>
2022-10-29 18:21:52 -0400

    Added graphql and storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,18 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "api": {
+        "reactapp": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "mybucketforreact": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
